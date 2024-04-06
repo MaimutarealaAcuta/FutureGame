856352fb84f745336ed2952d9f2ff2bb9b4ecb99
Andrei - Petru Balea <andrei.balea@student.upt.ro>
2024-04-06 16:37:44 +0300

    main menu

---
+++ Assets/GUI/Scripts/MainMenuScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuPanel;
+
+    [SerializeField]
+    private GameObject optionsPanel;
+
+    [SerializeField]
+    private GameObject creditsPanel;
+
+    bool isOptionsActive = false;
+    bool isCreditsActive = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region MainMenu
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+    }
+    #endregion
+
+    #region OptionsMenu
+    public void UpdateMasterVolume(float volume)
+    {
+        Debug.Log("Master Volume: " + volume);
+    }
+    #endregion
+
+    public void ToggleOptions()
+    {
+        Debug.Log("Toggle Options");
+
+        isOptionsActive = !isOptionsActive;
+        mainMenuPanel.SetActive(!isOptionsActive);
+        optionsPanel.SetActive(isOptionsActive);
+    }
+
+    public void ToggleCredits()
+    {
+        Debug.Log("Toggle Credits");
+
+        isCreditsActive = !isCreditsActive;
+        mainMenuPanel.SetActive(!isCreditsActive);
+        creditsPanel.SetActive(isCreditsActive);
+    }
+
+}
---
+++ Assets/GameSettings/GameManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : ScriptableObject
+{
+    public float MasterVolume = 1.0f;
+}
